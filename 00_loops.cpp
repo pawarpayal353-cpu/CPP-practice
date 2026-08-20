1775ba4028260bbdbd69e4f4f76b83977268f443
Dev Payal <pawarpayal353@gmail.com>
2026-08-20 15:29:52 +0530

    Add files via upload

---
+++ 00_loops.cpp
@@ -0,0 +1,28 @@
+/*
+LOOPS - They are block of statements which keeps on exicuiting repeatedly
+1.for loop= when we know range 
+2.while loop= when we dont know range
+3.do-while loop= when we want the code to be executed atleast once
+*/
+
+
+// for (initialization; condition; increment)
+// {
+// loop body(c++ code);
+// }
+
+
+// declaration
+// while (condition)
+// {
+//     initialization;
+//     increment;
+// }
+
+
+// declaration
+// do
+// {
+//     /* code */
+// } while (condition);
+
